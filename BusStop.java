9a80d175ff68efd61b52657e579174584c83e603
Robert Brice <rob.a.brice@gmail.comC>
2017-03-06 17:28:29 +0000

    improving length of queue method

--- BusStop.java
+++ BusStop.java
@@ -1,18 +1,45 @@
 class BusStop{
 
-private String location;
-private int = lengthOfQueue;
-private Passenger[] people;
+  private String location;
+  private Passenger[] people;
 
-public BusStop(String location, int lengthOfQueue){
-this.location = location;
-this.lengthOfQueue = lengthOfQueue;
-this.people = new Passenger[this.lengthOfQueue]
+  public BusStop(String location){
+    this.location = location;
+    this.people = new Passenger[15];
+  }
+
+  public String location(){
+    return this.location;
+  }
+
+
+
+  // public int lengthOfQueue(){
+  //   return people.length;
+  // } no...thats wrong as the people in the queue will queue up tosome maximum and will stop when it reches an uaceptable level
+
+
+
+
+    int count = 0;
+    for (Passenger passenger : seats) {
+      if (passenger != null){
+        count++;
+      }
+    }
+    return count;
+  }
+
+
+public void personGetsOnBus(){
+if (lengthOfQueue == 0) {
+  return;
 }
+int index = lengthOfQueue();
+people[index] = null;
 
 
-
-
+}
 
 
 
--- BusStopTest.java
+++ BusStopTest.java
@@ -2,8 +2,37 @@ import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
 
-public class BusStopTest{
+public class BusStopTest {
   BusStop busStop;
   Passenger passenger;
 
+  @Before
+  public void before() {
+    busStop = new BusStop("Castle Terrace", 16);
+    passenger = new Passenger();
+  }
+
+  @Test
+  public void hasLocation(){
+    assertEquals("Castle Terrace", busStop.location());
+  }
+
+  @Test
+  public void lengthOfQueue(){
+    assertEquals(16, busStop.lengthOfQueue());
+  }
+
+@Test
+public void personGetsOnBus(){
+busStop.personGetsOnBus();
+assertEquals(15, busStop.lengthOfQueue());
+
 }
+
+
+
+
+}
+
+
+
